20912a989782c89125a4b7044780defdae226cc0
LC <59321122+changliu17@users.noreply.github.com>
2020-05-21 15:02:18 -0400

    Update directlet_surface.py

--- directlet_surface.py
+++ directlet_surface.py
@@ -10,7 +10,7 @@ surface = plt.subplot(231,projection='3d') #  subplot 1
 x1 = np.array([0, 1, 0])
 y1 = np.array([1, 0, 0])
 z1 = np.array([0, 0, 1])  # cornor of triangle surface
-#surface.scatter(x1,y1,z1) # cornor point
+
 
 #create surface
 # 1. create vertices from points
@@ -20,12 +20,13 @@ srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
 # 3. add polygon to the figure (current axes)
 ax = plt.gca().add_collection3d(srf)
 
-# dirichlet
+# create dirichlet distribution data
 rng = np.random.default_rng()  # random PCG64 generator
-alpha = [0.2, 0.2, 0.2] #set alpha
+alpha = [0.2, 0.2, 0.2]   #set alpha
 s = rng.dirichlet(alpha=alpha, size=1500).transpose()
 ax = plt.gca(projection='3d')
-ax.scatter(s[0], s[1], s[2], c='#0059b3', s =2) #scatter with color
+ax.scatter(s[0], s[1], s[2], c='#0059b3', s =2) 
+
 # plot setting
 ax.view_init(30, 60)  #elev 30° azim 60°
 ax.set_title("α(0.2, 0.2, 0.2)") # set title
@@ -35,57 +36,61 @@ ax.set_zlabel('Z')
 
 
 
-surface=plt.subplot(232, projection='3d') #subplot 2
-#surface.scatter(x1,y1,z1) # cornor point
+surface=plt.subplot(232, projection='3d')    #subplot 2
 
+# create surface
+# 1. create vertices from points
 verts = [list(zip(x1, y1, z1))]
 # 2. create 3d polygons and specify parameters
 srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
 # 3. add polygon to the figure (current axes)
 ax = plt.gca().add_collection3d(srf)
-surface = plt.gca().add_collection3d(srf) #create surface
+surface = plt.gca().add_collection3d(srf)   #create surface
 
-#dirichlet
-rng = np.random.default_rng()  # random PCG64 generator
-alpha = [3, 3, 3] #set alpha
+# create dirichlet distribution data
+rng = np.random.default_rng()    # random PCG64 generator
+alpha = [3, 3, 3]   #set alpha
 s = rng.dirichlet(alpha=alpha, size=1500).transpose()
 ax = plt.gca(projection='3d')
-ax.scatter(s[0], s[1], s[2], c='#0059b3', s = 2) #scatter with color
-#plot setting
-ax.view_init(30, 60)  #elev 30° azim 60°
-ax.set_title("α=(3, 3, 3)") #set title
-ax.set_xlabel('X') #set label name
+ax.scatter(s[0], s[1], s[2], c='#0059b3', s = 2)  
+
+# plot setting
+ax.view_init(30, 60)  # elev 30° azim 60°
+ax.set_title("α=(3, 3, 3)")    #set title
+ax.set_xlabel('X')    #set label name
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
 
+surface=plt.subplot(233, projection='3d')   #subplot 3
 
-surface=plt.subplot(233, projection='3d') #subplot 3
-#surface.scatter(x1,y1,z1) # cornor point
-
+# create surface
+# 1. create vertices from points
 verts = [list(zip(x1, y1, z1))]
 # 2. create 3d polygons and specify parameters
 srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
 # 3. add polygon to the figure (current axes)
 ax = plt.gca().add_collection3d(srf)
-surface = plt.gca().add_collection3d(srf) #create surface
+surface = plt.gca().add_collection3d(srf)   #create surface
 
-#dirichlet
+# create dirichlet distribution data
 rng = np.random.default_rng()  # random PCG64 generator
 alpha = [9, 9, 9] #set alpha
 s = rng.dirichlet(alpha=alpha, size=1500).transpose()
 ax = plt.gca(projection='3d')
-ax.scatter(s[0], s[1], s[2], c='#0059b3', s = 2) #scatter with color
-#plot setting
-ax.view_init(30, 60)  #elev 30° azim 60°
-ax.set_title("α=(9, 9, 9)") #set title
-ax.set_xlabel('X') #set label name
+ax.scatter(s[0], s[1], s[2], c='#0059b3', s = 2)
+
+# plot setting
+ax.view_init(30, 60)  # elev 30° azim 60°
+ax.set_title("α=(9, 9, 9)")   #set title
+ax.set_xlabel('X')    #set label name
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
 surface=plt.subplot(234, projection='3d') #subplot 4
-#surface.scatter(x1,y1,z1) # cornor point
 
+# create surface
+# 1. create vertices from points
 verts = [list(zip(x1, y1, z1))]
 # 2. create 3d polygons and specify parameters
 srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
@@ -93,69 +98,50 @@ srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
 ax = plt.gca().add_collection3d(srf)
 surface = plt.gca().add_collection3d(srf) #create surface
 
-#dirichlet
+# create dirichlet distribution data
 rng = np.random.default_rng()  # random PCG64 generator
 alpha = [2, 6, 10] #set alpha
 s = rng.dirichlet(alpha=alpha, size=1500).transpose()
 ax = plt.gca(projection='3d')
-ax.scatter(s[0], s[1], s[2], c='#0059b3', s=2) #scatter with color
-#plot setting
+ax.scatter(s[0], s[1], s[2], c='#0059b3', s=2)   
+
+# plot setting
 ax.view_init(30, 60)  #elev 30° azim 60°
-ax.set_title("α=(2, 6, 11)") #set title
-ax.set_xlabel('X') #set label name
+ax.set_title("α=(2, 6, 10)")   #set title
+ax.set_xlabel('X')   #set label name
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-
-surface=plt.subplot(234, projection='3d') #subplot 4
-#surface.scatter(x1,y1,z1) # cornor point
-
-verts = [list(zip(x1, y1, z1))]
-# 2. create 3d polygons and specify parameters
-srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
-# 3. add polygon to the figure (current axes)
-ax = plt.gca().add_collection3d(srf)
-surface = plt.gca().add_collection3d(srf) #create surface
-
-#dirichlet
-rng = np.random.default_rng()  # random PCG64 generator
-alpha = [2, 6, 10] #set alpha
-s = rng.dirichlet(alpha=alpha, size=1500).transpose()
-ax = plt.gca(projection='3d')
-ax.scatter(s[0], s[1], s[2], c='#0059b3', s=2) #scatter with color
-#plot setting
-ax.view_init(30, 60)  #elev 30° azim 60°
-ax.set_title("α=(2, 6, 10)") #set title
-ax.set_xlabel('X') #set label name
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
 
 surface=plt.subplot(235, projection='3d') #subplot 5
-#surface.scatter(x1,y1,z1) # cornor point
 
+# create surface
+# 1. create vertices from points
 verts = [list(zip(x1, y1, z1))]
 # 2. create 3d polygons and specify parameters
 srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
 # 3. add polygon to the figure (current axes)
 ax = plt.gca().add_collection3d(srf)
-surface = plt.gca().add_collection3d(srf) #create surface
+surface = plt.gca().add_collection3d(srf)   #create surface
 
-#dirichlet
-rng = np.random.default_rng()  # random PCG64 generator
+# create dirichlet distribution data
+rng = np.random.default_rng()    # random PCG64 generator
 alpha = [14, 9, 5] #set alpha
 s = rng.dirichlet(alpha=alpha, size=1500).transpose()
 ax = plt.gca(projection='3d')
-ax.scatter(s[0], s[1], s[2], c='#0059b3', s=2) #scatter with color
+ax.scatter(s[0], s[1], s[2], c='#0059b3', s=2) 
+
 #plot setting
-ax.view_init(30, 60)  #elev 30° azim 60°
-ax.set_title("α=(14, 9, 5)") #set title
-ax.set_xlabel('X') #set label name
+ax.view_init(30, 60)    # elev 30° azim 60°
+ax.set_title("α=(14, 9, 5)")   #set title
+ax.set_xlabel('X')   # set label name
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
-surface=plt.subplot(236, projection='3d') #subplot 6
-#surface.scatter(x1,y1,z1) # cornor point
 
+surface=plt.subplot(236, projection='3d') #subplot 6
+
+# create surface
+# 1. create vertices from points
 verts = [list(zip(x1, y1, z1))]
 # 2. create 3d polygons and specify parameters
 srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
@@ -163,16 +149,17 @@ srf = Poly3DCollection(verts, alpha=.25, facecolor='#ffffb3')
 ax = plt.gca().add_collection3d(srf)
 surface = plt.gca().add_collection3d(srf) #create surface
 
-#dirichlet
-rng = np.random.default_rng()  # random PCG64 generator
+# create dirichlet distribution data
+rng = np.random.default_rng()   # random PCG64 generator
 alpha = [6, 2, 6] #set alpha
 s = rng.dirichlet(alpha=alpha, size=1500).transpose()
 ax = plt.gca(projection='3d')
-ax.scatter(s[0], s[1], s[2], c='#0059b3', s=2) #scatter with color
+ax.scatter(s[0], s[1], s[2], c='#0059b3', s=2)   #scatter with color
+
 #plot setting
-ax.view_init(30, 60)  #elev 30° azim 60°
-ax.set_title("α=(6, 2, 6)") #set title
-ax.set_xlabel('X') #set label name
+ax.view_init(30, 60)    #elev 30° azim 60°
+ax.set_title("α=(6, 2, 6)")   #set title
+ax.set_xlabel('X')   #set label name
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
